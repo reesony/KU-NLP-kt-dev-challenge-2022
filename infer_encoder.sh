40a6789329459853ae7e4ee2679a39d78970dcfd
SUNGYOON LEE <77087144+reesony@users.noreply.github.com>
2022-11-06 22:03:14 +0900

    Update infer_encoder.sh

--- infer_encoder.sh
+++ infer_encoder.sh
@@ -3,4 +3,4 @@ CUDA_VISIBLE_DEVICES=1 python infer_encoder.py \
 --model_name_or_path ./encoder_only_models \
 --test_file ./data/test1.csv \
 --test_model ./models/encoder/0.0001_16_4_0.1/checkpoint-2082 \
---data_path ./data/klue_ner_test_20.txt
+--data_path ./data/test.txt
